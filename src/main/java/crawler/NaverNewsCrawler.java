bfd45ed3d768067b4c64273e76a36e0ea50cd684
voiciphil <voiciphil@gmail.com>
2021-04-18 13:43:12 +0900

    네이버 뉴스 크롤링 안 되는 문제 수정

--- src/main/java/crawler/NaverNewsCrawler.java
+++ src/main/java/crawler/NaverNewsCrawler.java
@@ -13,10 +13,9 @@ public class NaverNewsCrawler implements Crawler {
 
     private static NaverNewsCrawler instance;
 
-    private static final String URL1 = "https://search.naver.com/search.naver?&where=news&query=";
-    private static final String URL2 = "&sm=tab_pge&sort=0&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid=&nso=so:r,p:all,a:all&mynews=0&cluster_rank=24&start=";
-    private static final String URL3 = "&refresh_start=0";
-    private static final String SELECTOR = ".type01 dl dt a";
+    private static final String URL1 = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query=";
+    private static final String URL2 = "&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=24&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=";
+    private static final String SELECTOR = ".news_tit";
     private static final int PAGES = 3;
     private static final int ELEMENTS_PER_PAGE = 10;
 
@@ -46,10 +45,10 @@ public class NaverNewsCrawler implements Crawler {
         try {
             for (int page = 1; page <= PAGES * ELEMENTS_PER_PAGE; page += ELEMENTS_PER_PAGE) {
                 sleep(500);
-                Document document = Jsoup.connect(URL1 + searchKeyword + URL2 + page + URL3).get();
+                Document document = Jsoup.connect(URL1 + searchKeyword + URL2 + page).get();
                 Elements elements = document.select(SELECTOR);
                 for (Element element : elements) {
-                    result.add(Website.of(element.text(), element.attr("href")));
+                    result.add(Website.of(element.attr("title"), element.attr("href")));
                 }
             }
         } catch (IOException e) {
